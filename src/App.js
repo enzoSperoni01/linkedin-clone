5a360da6046bcbe5fe265b839c0dc079ddb89539
Enzo Speroni <speronienzo20@gmail.com>
2023-01-27 00:13:31 -0300

    Moving forward with the 'Login' Section

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import Header from "./Header";
 import Sidebar from './Sidebar';
 import Feed from "./Feed";
 import './App.css';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Login from './Login';
 
 function App() {
+  const user = useSelector(selectUser); 
+
   return (
     <div className="app">
-      {/* Header */}
-      <Header />
 
-      {/* App Body */}
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* Widgets */}
-      </div>
+      { !user ? <Login /> : (
+        <>
+          <Header />
+          <div className="app__body">
+              <Sidebar />
+              <Feed />
+              {/* Widgets */}
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from './features/userSlice'
+import { auth } from './firebase'
+import './Login.css'
+
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [profilePic, setProfilePic] = useState("");
+    const dispatch = useDispatch();
+    
+    const loginToApp = e => {
+        e.preventDefault();
+    };
+    const register = () => {
+        if(!name) {
+            return alert('Please enter a full name!');
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .then( userAuth => {
+                userAuth.user.updateProfile({
+                    displayName: name,
+                    photoURL: profilePic
+                })
+                .then(() => {
+                    dispatch(
+                        login({
+                            email: userAuth.user.email,
+                            uid: userAuth.user.uid,
+                            displayName: name,
+                            photoUrl: profilePic
+                        }
+                    ))
+                })
+            })
+        .catch(error => alert(error))
+    };
+    
+    return (
+        <div className='login'>
+            <img src="https://cdn-icons-png.flaticon.com/512/372/372399.png" alt="Logo Linkedin" />
+
+            <form>
+                <input value={name} onChange={e => setName(e.target.value)} placeholder='Full name (Required if registering)' type="text" />
+                <input value={profilePic} onChange={e => setProfilePic(e.target.value)} placeholder='Profile Pic URL (Optional)' type="text" />
+                <input value={email} onChange={e => setEmail(e.target.value)} placeholder='Your email...' type="email" />
+                <input value={password} onChange={e => setPassword(e.target.value)} placeholder='Type your password...' type="password" />
+
+                <button type='submit' onClick={loginToApp}>Sign In</button>
+            </form>
+
+            <p>
+                Not a member?{' '}
+                <span className='login__register' onClick={register}>
+                    Register Now!
+                </span>
+            </p>
+        </div>
+    )
+}
+
+export default Login;
